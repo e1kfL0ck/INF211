e4ad189757e018146c6a54897cb9489a77a45cb2
Maxime Pere <maxime.pere92@gmail.com>
2025-03-14 22:13:32 +0100

    JobOfferMessage

--- src/main/java/fr/atlantique/imt/inf211/jobmngt/controller/MessageController.java
+++ src/main/java/fr/atlantique/imt/inf211/jobmngt/controller/MessageController.java
@@ -4,6 +4,7 @@ import fr.atlantique.imt.inf211.jobmngt.entity.JobOfferMessage;
 import fr.atlantique.imt.inf211.jobmngt.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +17,7 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
+    @GetMapping(value = "")
     public ModelAndView getAllMessages() {
         ModelAndView mav = new ModelAndView("message.html");
         List<JobOfferMessage> messages = messageService.listOfMessages();
